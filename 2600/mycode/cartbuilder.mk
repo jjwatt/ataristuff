36c1873a12d2ffab7990304d207a41cbf2a674fa
Jesse Wattenbarger <jwatt@broken.watch>
2020-01-31 15:33:37 -0500

    cartbuilder.mk will now copy include files to the proj dir.

--- 2600/mycode/cartbuilder.mk
+++ 2600/mycode/cartbuilder.mk
@@ -2,6 +2,15 @@ TARGET = $(cart).bin
 ASMFILES = $(wildcard *.asm)
 MD5 = $(shell cat $(TARGET).md5)
 
+vcs.h:
+	cp ../vcs.h ./
+
+macro.h:
+	cp ../macro.h ./
+
+stella.pro.in:
+	cp ../stella.pro.in ./
+
 stella.pro: stella.pro.in $(TARGET).md5
 	sed s/%%md5%%/$(MD5)/g stella.pro.in > $@
 
@@ -17,8 +26,11 @@ all: $(TARGET) $(TARGET).md5 stella.pro
 
 clean:
 	rm $(TARGET)
-  rm $(TARGET).md5
-  rm stella.pro
+	rm $(TARGET).md5
+	rm stella.pro
 
 run:
-	stella -tv.scanlines 50 -tv.filter 1 -grabmouse 0 $(TARGET)
+	stella -tv.scanlines 50 -tv.filter 1 \
+		-grabmouse 0 -logtoconsole 1 -statedir . \
+		-propsfile ./stella.pro \
+		$(TARGET)
