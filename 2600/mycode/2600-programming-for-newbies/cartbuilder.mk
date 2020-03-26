3bedd7d7347c634a3762358e7e3580793a8db7b1
Jesse Wattenbarger <jwatt@broken.watch>
2020-03-26 15:53:28 -0400

    Add code from Atari 2600 Programming for Newbies Session 13.
    - Playfield stuff

---
+++ 2600/mycode/2600-programming-for-newbies/cartbuilder.mk
@@ -0,0 +1,36 @@
+TARGET = $(cart).bin
+ASMFILES = $(wildcard *.asm)
+MD5 = $(shell cat $(TARGET).md5)
+
+all: $(TARGET) $(TARGET).md5 stella.pro
+
+vcs.h:
+	cp ../vcs.h ./
+
+macro.h:
+	cp ../macro.h ./
+
+stella.pro.in:
+	cp ../stella.pro.in ./
+
+stella.pro: stella.pro.in $(TARGET).md5
+	sed s/%%md5%%/$(MD5)/g stella.pro.in > $@
+
+$(TARGET): $(ASMFILES)
+	dasm $^ -f3 -v0 -o$@
+
+$(TARGET).md5: $(TARGET)
+	md5sum $(TARGET) | cut -d" " -f1 > $@
+
+.PHONY: all run clean
+
+clean:
+	rm $(TARGET)
+	rm $(TARGET).md5
+	rm stella.pro
+
+run:
+	stella -tv.scanlines 50 -tv.filter 1 \
+		-grabmouse 0 -logtoconsole 1 -statedir . \
+		-propsfile ./stella.pro \
+		$(TARGET)
